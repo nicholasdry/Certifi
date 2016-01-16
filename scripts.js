97053914701ccc32373293e5404e58b93442741d
Nicholas Dry <nicholasdry@me.com>
2016-01-16 14:57:44 -0500

    Added template file

---
+++ scripts.js
@@ -0,0 +1,14 @@
+// template question answers
+
+function checkscore() {
+
+      var score = 0;
+
+      if (document.getElementById('q1') == PUT ANSWER HERE) {
+            score++;
+      }
+
+      // make the other if-else statements for other questions
+
+      alert(score);
+}
