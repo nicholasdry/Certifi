4c2435528de7d96676fb51e634b0f8d2327104a2
Brandon Au <ba448@drexel.edu>
2016-01-16 16:18:08 -0500

    Updated scripts.js and template.html

--- scripts.js
+++ scripts.js
@@ -1,14 +1,26 @@
 // template question answers
 
-function checkscore() {
+function checkScore() {
 
       var score = 0;
 
-      if (document.getElementById('q1') == PUT ANSWER HERE) {
-            score++;
+      if (document.getElementById("q1").value == "19") {
+            score = score + 1;
       }
+	  if (document.getElementById("q2").value == "65"){
+		  score++;
+	  }
+	  if (document.getElementById("q3").value == "15"){
+		  score++;
+	  }
+	  if (document.getElementById("q4").value == "-273"){
+		  score++;
+	  }
+	  if (document.getElementById("q5").value == "2190"){
+		  score++;
+	  }
 
       // make the other if-else statements for other questions
 
-      alert(score);
+     alert("Your score is " + score + ". (5 is a perfect score)");
 }
