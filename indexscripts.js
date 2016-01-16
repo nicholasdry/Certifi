fc2b54baf92d06161cbaeb63d00c550acc197158
Nicholas Dry <nicholasdry@me.com>
2016-01-16 16:20:16 -0500

    Updated CSS

---
+++ indexscripts.js
@@ -0,0 +1,21 @@
+// index scripts
+
+function showJava() {
+      document.getElementById('output').innerHTML = "Check spelling or re-enter destination.";
+}
+
+function showHTML() {
+
+}
+
+function showJS() {
+
+}
+
+function showCSS() {
+
+}
+
+function showMath() {
+
+}
