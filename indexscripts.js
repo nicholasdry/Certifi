df58304a63398843bf8fec2041a681d9deb2b1cd
Nicholas Dry <nicholasdry@me.com>
2016-01-16 16:46:07 -0500

    Updated JS

--- indexscripts.js
+++ indexscripts.js
@@ -1,21 +1,21 @@
 // index scripts
 
 function showJava() {
-      document.getElementById('output').innerHTML = "Check spelling or re-enter destination.";
+      document.getElementById('content').innerHTML = "<a>Beginner</a><br><a>Intermediate</a><br><a>Advanced</a>";
 }
 
 function showHTML() {
-
+      document.getElementById('content').innerHTML = "<a>Beginner</a><br><a>Intermediate</a><br><a>Advanced</a>";
 }
 
 function showJS() {
-
+      document.getElementById('content').innerHTML = "<a>Beginner</a><br><a>Intermediate</a><br><a>Advanced</a>";
 }
 
 function showCSS() {
-
+      document.getElementById('content').innerHTML = "<a>Beginner</a><br><a>Intermediate</a><br><a>Advanced</a>";
 }
 
 function showMath() {
-
+      document.getElementById('content').innerHTML = "<a>Beginner</a><br><a>Intermediate</a><br><a>Advanced</a>";
 }
