f2eb6efe54195d2524abf73ce49d852304cfb731
Nicholas Dry <nicholasdry@me.com>
2016-01-16 21:10:47 -0500

    Updated Styles

--- html/beginnerQuiz.js
+++ html/beginnerQuiz.js
@@ -1,7 +1,6 @@
 function checkScore(){
 	var score = 0;
-	
-	
+
 	if(document.getElementById("q1").value.trim().toLowerCase() == "<b>DragonHacks</b>".toLowerCase()){
 		score++;
 	}
@@ -13,13 +12,12 @@ function checkScore(){
 	}
 	if(document.getElementById("q4").value.trim().toLowerCase() == "<hr/>" ){
 		score++;
-	}
-	else if(document.getElementById("q4").value.trim().toLowerCase() == "<hr>"){
+	} else if(document.getElementById("q4").value.trim().toLowerCase() == "<hr>"){
 		score++;
 	}
 	if(document.getElementById("q5").value.trim().toLowerCase() == "<h1>"){
 		score++;
 	}
-	
-	//alert("Your score is " + score + ". (5 is a perfect score)");
+
+	alert("Your score is " + score + ". (5 is a perfect score)");
 }
--- indexscripts.js
+++ indexscripts.js
@@ -1,7 +1,7 @@
 // index scripts
 
 function showJava() {
-      document.getElementById('content').innerHTML = "<a href='#'>Beginner</a><br><a href='#'>Intermediate</a><br><a href='#'>Advanced</a>";
+      document.getElementById('content').innerHTML = "<a href='html/beginnerQuiz.html'>Beginner</a><br><a href='#'>Intermediate</a><br><a href='#'>Advanced</a>";
 }
 
 function showHTML() {
