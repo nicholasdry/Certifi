cfee33a38a4b778aeaf6b0cc2714aae92dd935ca
Brandon Au <ba448@drexel.edu>
2016-01-17 06:49:11 -0500

    Beginner CSS and Advanced JS

--- css/beginnerQuiz.js
+++ css/beginnerQuiz.js
@@ -2,21 +2,51 @@ function checkScore(){
 	var score = 0;
 	
 	if(document.getElementById("q1").value.trim().toLowerCase() == "background-color: #5659AD".toLowerCase()){
+		document.getElementById("q1").style.backgroundColor = 'green'
 		score++;
 	}
 	else if(document.getElementById("q1").value.trim().toLowerCase() == "background-color: #5659AD;".toLowerCase()){
+		document.getElementById("q1").style.backgroundColor = 'green'
 		score++;
+	}
+	else{
+		document.getElementById("q1").style.backgroundColor = 'red'
 	}
 	
 	if(document.getElementById("q2").value.trim().toLowerCase() == "p"){
+		document.getElementById("q2").style.backgroundColor = 'green'
 		score++;
 	}
-	
-	if(document.getElementById("q3").value.trim() == "p{
-		+ "font-family: Arial;" +
-	+ "}"){
-		score++;
+	else{
+		document.getElementById("q2").style.backgroundColor = 'red'
 	}
 	
-	alert("Your score is " + score + ".");
+	if(document.getElementById("q3").value.trim() == "font-family: Arial;"){
+		document.getElementById("q3").style.backgroundColor = 'green'
+		score++;
+	}
+	else{
+		document.getElementById("q3").style.backgroundColor = 'red'
+	}
+	
+	if(document.getElementById("q4").value.trim() == "/* */"){
+		document.getElementById("q4").style.backgroundColor = 'green'
+		score++;
+	}
+	else{
+		document.getElementById("q4").style.backgroundColor = 'red'
+	}
+	
+	if(document.getElementById("q5").value.trim() == "font-weight: bold;"){
+		document.getElementById("q5").style.backgroundColor = 'green';
+		score++;
+	}
+	else{
+		document.getElementById.style.backgroundColor = 'red';
+	}
+	//alert("Your score is " + score + ".");
+}
+
+function printOutPage(){
+	window.print();
 }
--- js/advancedscripts.js
+++ js/advancedscripts.js
@@ -1,29 +1,53 @@
-// template question answers
-
 function checkScore() {
 
       var score = 0;
 
-      if(document.getElementById("q1").value.trim().toLowerCase() == "2x3"){
-		score++;
-	}
-	if(document.getElementById("q2").value.trim().toLowerCase() == '<ul type="square"></ul>'.toLowerCase()){
-		score++;
-	}
-	else if(document.getElementById("q2").value.trim().toLowerCase() == '<ul type="circle"></ul>'.toLowerCase()){
-		score++;
-	}
-	if(document.getElementById("q3").value.trim().toLowerCase() == '<a href = "somelink.com"><img src = "something.gif"></a>'.toLowerCase()){
-		score++;
-	}
-	if(document.getElementById("q4").value.trim().toLowerCase() == '<ol type="a"></ol>'.toLowerCase() ){
-		score++;
-	}
-	if(document.getElementById("q5").value.trim().toLowerCase() == "<th></th>"){
-		score++;
-	}
+      if(document.getElementById("q1").value.trim().toLowerCase() == "11"){
+        document.getElementById("q1").style.backgroundColor = 'green';
+                score++;
+        }
+        else    {
+                        document.getElementById("q1").style.backgroundColor = 'red';
+
+        }
+
+        if(document.getElementById("q2").value.trim()== "3"){
+                document.getElementById("q2").style.backgroundColor = 'green';
+                score++;
+        }
+        else
+                {
+                        document.getElementById("q2").style.backgroundColor = 'red';
+                }
+
+        if(document.getElementById("q3").value.trim() == "4"){
+                document.getElementById("q3").style.backgroundColor = "green";
+
+                score++;
+        }
+        else
+                {
+                        document.getElementById("q3").style.backgroundColor = 'red';
+
+                }
+
+        if(document.getElementById("q4").value.trim().toLowerCase() == "true".toLowerCase() ){
+                document.getElementById("q4").style.backgroundColor = "green";
+
+                score++;
+        }
+        else    {
+                document.getElementById("q4").style.backgroundColor = "red";
+                }
+
+        if(document.getElementById("q5").value.trim() == "0.30000000000000004"){
+                document.getElementById("q5").style.backgroundColor = "green";
+                score++;
+        }
+        else
+                document.getElementById("q5").style.backgroundColor = "red";
 
       // make the other if-else statements for other questions
 
-     alert("Your score is " + score + ". (5 is a perfect score)" + " For " + window.title);
+     //alert("Your score is " + score + ". (5 is a perfect score)" + " For " + window.title);
 }
