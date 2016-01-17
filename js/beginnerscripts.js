31183f96b6f15304923d287aa9ba26c6325ceab6
jea74 <jea74@drexel.edu>
2016-01-17 04:13:10 -0800

    made the green and red text boxes

--- js/beginnerscripts.js
+++ js/beginnerscripts.js
@@ -5,47 +5,74 @@ function checkScore() {
       var score = 0;
 
       if (document.getElementById("q1").value.trim().toLowerCase() == '<script LANGUAGE="JavaScript"></script>'.toLowerCase()){
+		  document.getElementById("q1").style.backgroundColor = 'green';
           score++;
       }
 	  else if (document.getElementById("q1").value.trim() == '<script></script>'){
+		  document.getElementById("q1").style.backgroundColor = 'green';
           score++;
       }
+	  else{
+		  document.getElementById("q1").style.backgroundColor = 'red';
+	  }
 	  
 	if(document.getElementById("q2").value.trim() == 'var x = 69'){
+		document.getElementById("q2").style.backgroundColor = 'green';
 		score++;
 	}
 	else if(document.getElementById("q2").value.trim() == 'var x = 69;'){
+		document.getElementById("q2").style.backgroundColor = 'green';
 		score++;
+	}
+	else{
+		document.getElementById("q2").style.backgroundColor = 'red';
 	}
 
 	if(document.getElementById("q3").value.trim() == 'document.write("Where have all the flowers gone?")'){
+		document.getElementById("q3").style.backgroundColor = 'green';
 		score++;
 	}
 	else if(document.getElementById("q3").value.trim() == 'document.write("Where have all the flowers gone?");'){
+		document.getElementById("q3").style.backgroundColor = 'green';
 		score++;
+	}
+	else{
+		document.getElementById("q3").style.backgroundColor = 'red';
 	}
 	
 	if(document.getElementById("q4").value.trim() == 'var y = 23*3;' ){
+		document.getElementById("q4").style.backgroundColor = 'green';
 		score++;
 	}
 	else if(document.getElementById("q4").value.trim() == 'var y = 23*3' ){
+		document.getElementById("q4").style.backgroundColor = 'green';
 		score++;
 	}
 	else if(document.getElementById("q4").value.trim() == 'var y = 3*23' ){
+		document.getElementById("q4").style.backgroundColor = 'green';
 		score++;
 	}
 	else if(document.getElementById("q4").value.trim() == 'var y = 3*23;' ){
+		document.getElementById("q4").style.backgroundColor = 'green';
 		score++;
+	}
+	else{
+		document.getElementById("q4").style.backgroundColor = 'red';
 	}
 	
 	if(document.getElementById("q5").value.trim() == 'document.write(y)'){
+		document.getElementById("q5").style.backgroundColor = 'green';
 		score++;
 	}
 	else if(document.getElementById("q5").value.trim() == 'document.write(y);'){
+		document.getElementById("q5").style.backgroundColor = 'green';
 		score++;
+	}
+	else{
+		document.getElementById("q5").style.backgroundColor = 'red';
 	}
 
       // make the other if-else statements for other questions
 
-     //alert("Your score is " + score + ". (5 is a perfect score)");
+     alert("Your score is " + score + ". (5 is a perfect score)");
 }
--- js/intscripts.js
+++ js/intscripts.js
@@ -5,29 +5,51 @@ function checkScore() {
       var score = 0;
 
       if (document.getElementById("q1").value.trim() == 'document.write("Today\'s forecast is " + weather);'){
+		  document.getElementById("q1").style.backgroundColor = 'green';
           score++;
       }
 	  else if (document.getElementById("q1").value.trim() == 'document.write("Today\'s forecast is " + weather)'){
+		  document.getElementById("q1").style.backgroundColor = 'green';
           score++;
       }
+	  else{
+		  document.getElementById("q1").style.backgroundColor = 'red';
+	  }
 	  
 	if(document.getElementById("q2").value.trim().toLowerCase == 'word.substring(5,8)'.toLowerCase){
+		document.getElementById("q2").style.backgroundColor = 'green';
 		score++;
 	}
 	else if(document.getElementById("q2").value.trim().toLowerCase == 'word.substring(5,8);'.toLowerCase){
+		document.getElementById("q2").style.backgroundColor = 'green';
 		score++;
+	}
+	else{
+		document.getElementById("q2").style.backgroundColor = 'red';
 	}
 
 	if(document.getElementById("q3").value.trim() == 'x is less than 5!'){
+		document.getElementById("q3").style.backgroundColor = 'green';
 		score++;
+	}
+	else{
+		document.getElementById("q3").style.backgroundColor = 'red';
 	}
 
 	if(document.getElementById("q4").value.trim().toLowerCase() == 'onMouseover'.toLowerCase()){
+		document.getElementById("q4").style.backgroundColor = 'green';
 		score++;
+	}
+	else{
+		document.getElementById("q4").style.backgroundColor = 'red';
 	}
 
 	if(document.getElementById("q5").value.trim() == 'Dragons are not a fruit, unless you\'re talking about dragon fruit.'){
+		document.getElementById("q5").style.backgroundColor = 'green';
 		score++;
+	}
+	else{
+		document.getElementById("q5").style.backgroundColor = 'red';
 	}
 
 
